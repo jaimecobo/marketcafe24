c1f2d494a22419595deea0fe91d2532264e40c86
Jaime J Cobo <jaimecobo@outlook.com>
2021-08-30 20:14:02 -0600

    React Project marketcafe24 components directory + Dishes.js

---
+++ src/components/Dishes.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import '../css/Dishes.css';
+
+
+import tiny_horse_agency_gradient from '../images/Tiny-Horse-Agency-Gradient.jpg';
+import ad01 from '../images/ad01.jpg'
+import blackBigLogo from '../images/blackBigLogo.jpg'
+import hrs_services from '../images/hrs_services.jpg'
+import mainLogoClear from '../images/mainLogoClear.jpg'
+import Map from '../images/Map.jpg'
+import pics_four from '../images/pics_four.jpg'
+import servs_pay_opt from '../images/servs_pay_opt.jpg'
+import where_to from '../images/where_to.jpg'
+
+import blackBigLogo200x200 from '../images/blackBigLogo100x100.png'
+import clearLogo200x200 from '../images/clearLogo100x100.png'
+
+
+class Dishes extends Component {
+    // constructor() {
+    //     super();
+    // }
+
+    render() {
+        return (
+
+            <div id="gradientBar_home">
+
+                <div className="slider">
+              
+                    <div className="slide-track">
+
+                    <div className="slide">
+                            <img alt="" src={blackBigLogo200x200} />
+                         </div>
+
+                         <div className="slide">
+                            <img alt="" src={clearLogo200x200} />
+                         </div>
+
+                        <div className="slide">
+                            <img alt="" src={blackBigLogo200x200} />
+                         </div>
+
+                         <div className="slide">
+                            <img alt="" src={clearLogo200x200} />
+                         </div>
+
+                         <div className="slide">
+                            <img alt="" src={blackBigLogo200x200} />
+                         </div>
+
+                         <div className="slide">
+                            <img alt="" src={clearLogo200x200} />
+                         </div>
+
+
+                         <div className="slide">
+                            <img alt="" src={blackBigLogo200x200} />
+                         </div>
+
+                         <div className="slide">
+                            <img alt="" src={clearLogo200x200} />
+                         </div>
+
+                        <div className="slide">
+                            <img alt="" src={blackBigLogo200x200} />
+                         </div>
+
+                         <div className="slide">
+                            <img alt="" src={clearLogo200x200} />
+                         </div>
+
+                         <div className="slide">
+                            <img alt="" src={blackBigLogo200x200} />
+                         </div>
+
+                         <div className="slide">
+                            <img alt="" src={clearLogo200x200} />
+                         </div>
+
+                         <div className="slide">
+                            <img alt="" src={blackBigLogo200x200} /> 
+                         </div>
+
+                         <div className="slide">
+                         <img alt="" src={clearLogo200x200} />
+                         </div>
+
+                         <div className="slide">
+                            <img alt="" src={blackBigLogo200x200} /> 
+                         </div>
+
+                         <div className="slide">
+                         <img alt="" src={clearLogo200x200} />
+                         </div>
+
+                     </div>
+                 </div>
+            
+                <img alt="" src={tiny_horse_agency_gradient} />
+
+
+   
+
+            </div>
+    
+        );
+    }
+}
+
+export default Dishes;
+
+
