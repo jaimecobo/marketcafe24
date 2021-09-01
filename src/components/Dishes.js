6bf3eec27c22bd978331f1aa25d97c6b2e002f29
Jaime J Cobo <jaimecobo@outlook.com>
2021-08-31 22:59:06 -0600

    React project marketcafe24 - components/Dishes.js

--- src/components/Dishes.js
+++ src/components/Dishes.js
@@ -4,17 +4,36 @@ import '../css/Dishes.css';
 
 
 import tiny_horse_agency_gradient from '../images/Tiny-Horse-Agency-Gradient.jpg';
-import ad01 from '../images/ad01.jpg'
-import blackBigLogo from '../images/blackBigLogo.jpg'
-import hrs_services from '../images/hrs_services.jpg'
-import mainLogoClear from '../images/mainLogoClear.jpg'
-import Map from '../images/Map.jpg'
-import pics_four from '../images/pics_four.jpg'
-import servs_pay_opt from '../images/servs_pay_opt.jpg'
-import where_to from '../images/where_to.jpg'
+// import ad01 from '../images/ad01.jpg'
+// import blackBigLogo from '../images/blackBigLogo.jpg'
+// import hrs_services from '../images/hrs_services.jpg'
+// import mainLogoClear from '../images/mainLogoClear.jpg'
+// import Map from '../images/Map.jpg'
+// import pics_four from '../images/pics_four.jpg'
+// import servs_pay_opt from '../images/servs_pay_opt.jpg'
+// import where_to from '../images/where_to.jpg'
 
 import blackBigLogo200x200 from '../images/blackBigLogo100x100.png'
 import clearLogo200x200 from '../images/clearLogo100x100.png'
+
+import food01 from '../images/Google Screeshots/edited/Food01.png'
+import food02 from '../images/Google Screeshots/edited/Food02.png'
+import food03 from '../images/Google Screeshots/edited/Food03.png'
+import food04 from '../images/Google Screeshots/edited/Food04.png'
+import food05 from '../images/Google Screeshots/edited/Food05.png'
+import food06 from '../images/Google Screeshots/edited/Food06.png'
+import food07 from '../images/Google Screeshots/edited/Food07.png'
+import food08 from '../images/Google Screeshots/edited/Food08.png'
+import food09 from '../images/Google Screeshots/edited/Food09.png'
+import food10 from '../images/Google Screeshots/edited/Food10.png'
+import food11 from '../images/Google Screeshots/edited/Food11.png'
+import food12 from '../images/Google Screeshots/edited/Food12.png'
+import food13 from '../images/Google Screeshots/edited/Food13.png'
+import food14 from '../images/Google Screeshots/edited/Food14.png'
+import food15 from '../images/Google Screeshots/edited/Food15.png'
+import food16 from '../images/Google Screeshots/edited/Food16.png'
+import food17 from '../images/Google Screeshots/edited/Food17.png'
+
 
 
 class Dishes extends Component {
@@ -31,75 +50,99 @@ class Dishes extends Component {
               
                     <div className="slide-track">
 
-                    <div className="slide">
-                            <img alt="" src={blackBigLogo200x200} />
+                        <div className="slide">
+                           <img alt="" src={food01} />
+                            {/* <img alt="" src={blackBigLogo200x200} /> */}
                          </div>
 
                          <div className="slide">
-                            <img alt="" src={clearLogo200x200} />
+                           <img alt="" src={food02} />
+                            {/* <img alt="" src={clearLogo200x200} /> */}
                          </div>
 
                         <div className="slide">
-                            <img alt="" src={blackBigLogo200x200} />
+                           <img alt="" src={food03} />
+                            {/* <img alt="" src={blackBigLogo200x200} /> */}
                          </div>
 
                          <div className="slide">
-                            <img alt="" src={clearLogo200x200} />
+                           <img alt="" src={food04} />
+                            {/* <img alt="" src={clearLogo200x200} /> */}
                          </div>
 
                          <div className="slide">
-                            <img alt="" src={blackBigLogo200x200} />
+                           <img alt="" src={food05} />
+                            {/* <img alt="" src={blackBigLogo200x200} /> */}
                          </div>
 
                          <div className="slide">
-                            <img alt="" src={clearLogo200x200} />
-                         </div>
-
-
-                         <div className="slide">
-                            <img alt="" src={blackBigLogo200x200} />
+                           <img alt="" src={food06} />
+                            {/* <img alt="" src={clearLogo200x200} /> */}
                          </div>
 
                          <div className="slide">
-                            <img alt="" src={clearLogo200x200} />
+                           <img alt="" src={food07} />
+                            {/* <img alt="" src={blackBigLogo200x200} /> */}
+                         </div>
+
+                         <div className="slide">
+                           <img alt="" src={food08} />
+                            {/* <img alt="" src={clearLogo200x200} /> */}
                          </div>
 
                         <div className="slide">
-                            <img alt="" src={blackBigLogo200x200} />
+                           <img alt="" src={food09} />
+                            {/* <img alt="" src={blackBigLogo200x200} /> */}
                          </div>
 
                          <div className="slide">
-                            <img alt="" src={clearLogo200x200} />
+                           <img alt="" src={food10} />
+                            {/* <img alt="" src={clearLogo200x200} /> */}
                          </div>
 
                          <div className="slide">
-                            <img alt="" src={blackBigLogo200x200} />
+                           <img alt="" src={food11} />
+                            {/* <img alt="" src={blackBigLogo200x200} /> */}
                          </div>
 
                          <div className="slide">
-                            <img alt="" src={clearLogo200x200} />
+                           <img alt="" src={food12} />
+                            {/* <img alt="" src={clearLogo200x200} /> */}
                          </div>
 
                          <div className="slide">
-                            <img alt="" src={blackBigLogo200x200} /> 
+                           <img alt="" src={food13} />
+                            {/* <img alt="" src={blackBigLogo200x200} />  */}
                          </div>
 
                          <div className="slide">
-                         <img alt="" src={clearLogo200x200} />
+                           <img alt="" src={food14} />
+                           {/* <img alt="" src={clearLogo200x200} /> */}
                          </div>
 
                          <div className="slide">
-                            <img alt="" src={blackBigLogo200x200} /> 
+                           <img alt="" src={food15} />
+                            {/* <img alt="" src={blackBigLogo200x200} />  */}
                          </div>
 
                          <div className="slide">
-                         <img alt="" src={clearLogo200x200} />
+                           <img alt="" src={food16} />
+                           {/* <img alt="" src={clearLogo200x200} /> */}
                          </div>
+
+                         <div className="slide">
+                           <img alt="" src={food17} />
+                           {/* <img alt="" src={clearLogo200x200} /> */}
+                         </div>
+
+                         {/* <div className="slide">
+                           <img alt="" src={food17} />
+                         </div> */}
 
                      </div>
                  </div>
             
-                <img alt="" src={tiny_horse_agency_gradient} />
+                <img id="gradient01" alt="" src={tiny_horse_agency_gradient} />
 
 
    
@@ -110,6 +153,6 @@ class Dishes extends Component {
     }
 }
 
-export default Dishes;
+export default Dishes
 
 
