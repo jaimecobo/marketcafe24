1e949d86ca7bb81771bf8063156c08ec2a2e7903
Jaime J Cobo <jaimecobo@outlook.com>
2021-09-19 08:22:36 -0600

    React Project marketcafe24 - ContactUs component

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,74 +1,105 @@
-import React, { Component } from 'react';
+
+import React, {useState} from 'react';
+import {db} from './FirebaseConfig';
+import { collection, addDoc } from "firebase/firestore"; 
 import '../css/ContactUs.css';
 
 
-class ContactUS extends Component {
 
-    render() {
-        return (
+  const ContactUs = () =>{
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
 
 
+      async function handleSubmitContact(e){
 
-    <div id="formDiv">
-    <div id="formTitle">
-        <h2>
-          <span id="spanTitle">Nos encanta responder vuestras dudas, conocer vuestras opiniones, Escribid!</span>
-        </h2>
-    </div>
-
-    <form action="#" method="post">
-      <fieldset>
-
-        <ul id="ul_form" class="nav">
-          <li id="ul_li_label_form">
-            <label>NOMBRE *</label>
-          </li>
-
+      e.preventDefault();
       
-          <li id="ul_li_label_form_01">
-            <label>APELLIDO *</label>
-          </li>
 
-      
-          <li id="ul_li_label_form_01">
-            <label>CORREO ELECTRONICO *</label>
-          </li>
-        </ul>
+      try {
+        const docRef = await addDoc(collection(db, "contacts"), {
+          name: name,
+          email: email,
+          message: message
+        });
+        alert("Gracias por escribir!");
+        // console.log("Document written with ID: ", docRef.id);
+      } catch (error) {
+        // alert(error.message);
+        // console.error("Error adding document: ", error);
+      }
 
-        <ul id="ul_form" class="nav">
-          <li>
-            <input type="text" id="input_text" value=""/>
-          </li>
-          <li>
-            <input type="text" id="input_text" value=""/>
-          </li>
-          <li>
-            <input type="text" id="input_text" value=""/>
-          </li>
-        </ul>
+      setName('');
+      setEmail('');
+      setMessage('');
+    };
 
-        <ul id="ul_form" class="nav">
-          <li id="ul_li_text_area_title">
-            <label>¿COMO PODEMOS AYUDAR? *</label>
-          </li>
-        </ul>
 
-        <ul id="ul_form" class="nav">
-          <li>
-            <textarea cols="40" rows="7" id="avia_4_1"></textarea>
-          </li>
-        </ul>
 
-        <p class="form_element">
-          <input type="hidden" value="1" name="submit_form1"/>
-          <input id="formButton" type="submit" value="ENVIAR" class="button" data-sending-label="Sending"/>
-        </p>
-      </fieldset>
-    </form>
-  </div>
 
-);
-}
-}
+    return (
 
-export default ContactUS
+      <div id="formDiv">
+        <div id="formTitle">
+            <h2>
+              <span id="spanTitle">Nos encanta responder vuestras dudas, conocer vuestras opiniones, Escribid!</span>
+            </h2>
+        </div>
+
+        <form className="form" onSubmit={handleSubmitContact}>
+          <fieldset>
+
+            <ul id="ul_form" class="nav">
+              <li id="ul_li_label_form">
+                <label>NOMBRE *</label>
+              </li>
+
+          
+              {/* <li id="ul_li_label_form_01">
+                <label>APELLIDO *</label>
+              </li> */}
+
+          
+              <li id="ul_li_label_form_01">
+                <label>CORREO ELECTRONICO *</label>
+              </li>
+            </ul>
+
+            <ul id="ul_form" class="nav">
+              <li>
+                <input type="text" id="input_text" placeholder="Nombre" value={name} onChange={(e)=>setName(e.target.value)}/>
+              </li>
+              <li>
+                <input type="text" id="input_text" placeholder="Correo electrónico" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+              </li>
+              {/* <li>
+                <input type="text" id="input_text" value=""/>
+              </li> */}
+            </ul>
+
+            <ul id="ul_form" class="nav">
+              <li id="ul_li_text_area_title">
+                <label>¿COMO PODEMOS AYUDAR? *</label>
+              </li>
+            </ul>
+
+            <ul id="ul_form" class="nav">
+              <li>
+                <textarea cols="40" rows="7" id="avia_4_1" placeholder="..." value={message} onChange={(e)=>setMessage(e.target.value)}></textarea>
+              </li>
+            </ul>
+
+            <p class="form_element">
+              <input type="hidden" value="1" name="submit_form1"/>
+              <input id="formButton" type="submit" value="ENVIAR" class="button" data-sending-label="Sending"/>
+            </p>
+          </fieldset>
+        </form>
+      </div>
+
+    );
+  }
+
+export default ContactUs;
