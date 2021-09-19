918a3cae5f9a8d85f623fc8d5651585af1350f26
Jaime J Cobo <jaimecobo@outlook.com>
2021-09-19 08:23:00 -0600

    React Project marketcafe24 - Firebase component

--- src/components/FirebaseConfig.js
+++ src/components/FirebaseConfig.js
@@ -1,16 +1,20 @@
-import * as firebase from "firebase/app";
-import "firebase/auth";
 
-const firebaseConfig = firebase.initializeApp({
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MESUREMENT_ID
+
+const firebaseApp = initializeApp({
+
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+
 
 });
 
-export default firebaseConfig;
+const db = getFirestore();
+
+export {db};
