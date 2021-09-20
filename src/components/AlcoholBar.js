d08288ba48d1998c6a81cfbc39b1b1d94e5eb0bc
Jaime J Cobo <jaimecobo@outlook.com>
2021-09-19 21:54:32 -0600

    React Project marketcafe24 - AlcoholBar component corrected

--- src/components/AlcoholBar.js
+++ src/components/AlcoholBar.js
@@ -5,7 +5,7 @@ import '../css/AlcoholBar.css';
 import alcoholBar from '../images/edited/Alcohol01.png';
 
 
-import logo01 from '../images/edited/01_Logo01_3.png'
+// import logo01 from '../images/edited/01_Logo01_3.png'
 
 
 class AlcoholBar extends Component {
