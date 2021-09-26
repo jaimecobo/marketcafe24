e5102f814c6ad9e4547fe440d56f99c7dabe98ff
Jaime J Cobo <jaimecobo@outlook.com>
2021-09-26 01:07:41 -0600

    React Project marketcafe24 - Bernabeu.js

--- src/components/Bernabeu.js
+++ src/components/Bernabeu.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import '../css/Bernabeu.css';
-// import '../css/Dishes.css';
 import bernabeu from '../images/Bernabeu03.jpeg'
 import bernabeuOffers from '../images/BernabeuOffers03.svg'
-// import services01 from '..//images/Services01.svg'
-// import services02 from '..//images/Services02.svg'
+
 
 
 class Bernabeu extends Component {
@@ -14,9 +12,8 @@ class Bernabeu extends Component {
            
             <div class="image-container">
 
-                <img alt="" id="bernabeu" src={bernabeu} />
-                <img alt="" id="bernabeuOffers" src={bernabeuOffers}/>
-                {/* <img alt="" id="services02" src={services02}/> */}
+                <img className="img-fluid" alt="" id="bernabeu" src={bernabeu} />
+                <img className="img-fluid" alt="" id="bernabeuOffers" src={bernabeuOffers}/>
                 
             </div>
 
