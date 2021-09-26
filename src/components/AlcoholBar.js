28c4f1c305e758c1797dfa7d9a77f5bc951d1e21
Jaime J Cobo <jaimecobo@outlook.com>
2021-09-26 01:06:20 -0600

    React Project marketcafe24 - AlcoholBar

--- src/components/AlcoholBar.js
+++ src/components/AlcoholBar.js
@@ -5,7 +5,7 @@ import '../css/AlcoholBar.css';
 import alcoholBar from '../images/edited/Alcohol01.png';
 
 
-// import logo01 from '../images/edited/01_Logo01_3.png'
+// import logo01 from '../images/edited/01_Logo01_3.png\
 
 
 class AlcoholBar extends Component {
@@ -15,7 +15,7 @@ class AlcoholBar extends Component {
         return (
 
             <div className="image-container">
-                <img id="alcoholBar" alt="" src={alcoholBar} />
+                <img className="img-fluid" id="alcoholBar" alt="" src={alcoholBar} />
             </div>
 
         );
